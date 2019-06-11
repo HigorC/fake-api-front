0cb5397e99caa695fe3c54d95313662584b4d980
Higor Chaves <higor_oliveira@outlook.com>
2019-06-10 21:54:18 -0400

    Deploy no Heroku

--- src/components/Cadastro.vue
+++ src/components/Cadastro.vue
@@ -9,18 +9,55 @@
       rows="9"
     ></b-form-textarea>
 
-    <button class="btn btn-block btn-success btn-criar">Criar API Fake!</button>
+    <button class="btn btn-block btn-success btn-criar" @click="criarApi()">Criar API Fake!</button>
+
+    <b-modal
+      ref="modal-sucesso"
+      id="modal-1"
+      title="API Fake criada com sucesso!"
+      modal-cancel="false"
+    >
+      <div class="conteudo-modal">
+        Sua API está pronta para uso.
+        Acesse-a clicando
+        <a
+          target="_blank"
+          :href="linkCriado"
+        >Aqui</a>
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Cadastro",
   data: function() {
     return {
       rota: "",
-      retorno: ""
+      retorno: "",
+      linkCriado: ""
     };
+  },
+  methods: {
+    criarApi: function() {
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+      axios
+        .post(
+          `${proxyurl}https://fake-api-back.herokuapp.com/new/${this.rota}`,
+          JSON.parse(this.retorno)
+        )
+        .then(response => {
+          if (response.status == 203) {
+            this.linkCriado = response.data.rota;
+            this.$refs["modal-sucesso"].show();
+          }
+          console.log(response);
+        });
+    }
   }
 };
 </script>
